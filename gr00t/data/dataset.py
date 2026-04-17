349f6c1ee739f2e2907b95a9bd5b73b1071cedc0
silei <1403417493@qq.com>
2026-04-17 16:24:58 +0800

    add randomly sampling a start_offset of eps' steps

--- gr00t/data/dataset.py
+++ gr00t/data/dataset.py
@@ -426,7 +426,7 @@ class LeRobotSingleDataset(Dataset):
         """
         all_steps: list[tuple[int, int]] = []
         for trajectory_id, trajectory_length in zip(self.trajectory_ids, self.trajectory_lengths):
-            # randaomly sample a start offset for each trajectory, removing 0-30 frames of each episode
+            # randomly sample a start offset for each trajectory, removing 0-30 frames of each episode
             start_offset = min(np.random.randint(0, 31), max(0, trajectory_length - 1))
             for base_index in range(start_offset, trajectory_length):
                 all_steps.append((trajectory_id, base_index))
